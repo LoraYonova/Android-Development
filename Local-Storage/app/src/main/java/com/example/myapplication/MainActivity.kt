222b4a5cf02362a034adc59fa85b608cb02e6ec3
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2022-11-14 12:13:10 +0200

    Initial commit

---
+++ Local-Storage/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val sharedPrefs = getSharedPreferences(
+            "my_shared_pref",
+            Context.MODE_PRIVATE
+        )
+    }
+}
