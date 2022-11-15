ea837abfe9e1a4d46423a6d80cb60389209b6f8b
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2022-11-15 15:50:22 +0200

    Implement Data

--- Work-with-API-Homework/app/src/main/java/com/example/myapplication/Country.kt
+++ Work-with-API-Homework/app/src/main/java/com/example/myapplication/Country.kt
@@ -10,3 +10,12 @@ data class Flag(
     var svg: String,
     var png: String
 )
+
+data class CountryInfo(
+    var name: String,
+    var capital: String,
+    var flags: Flag,
+    var region: String,
+    var population: Int,
+    var area: Int
+)
