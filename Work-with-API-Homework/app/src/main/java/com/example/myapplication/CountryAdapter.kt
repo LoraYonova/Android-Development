7f7d2791ae96d6ed03b8ec9befcbbb72c37ea1a1
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2022-11-15 15:49:04 +0200

    Implement Adapter

--- Work-with-API-Homework/app/src/main/java/com/example/myapplication/CountryAdapter.kt
+++ Work-with-API-Homework/app/src/main/java/com/example/myapplication/CountryAdapter.kt
@@ -2,18 +2,21 @@ package com.example.myapplication
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.myapplication.databinding.CountryListBinding
+import com.google.android.material.snackbar.Snackbar
 
-class CountryAdapter(private val countries: List<Country>) : RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
+class CountryAdapter(private val countries: List<Country>) :
+    RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
 
     class CountryViewHolder(val binding: CountryListBinding) : RecyclerView.ViewHolder(binding.root)
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding = CountryListBinding.inflate(layoutInflater, parent, false)
+
         return CountryViewHolder(binding)
     }
 
@@ -24,11 +27,22 @@ class CountryAdapter(private val countries: List<Country>) : RecyclerView.Adapte
             capital = currentCountry.capital
 
             Glide
-                .with(root.context)
+                .with(this.root.context)
                 .load(currentCountry.flags.png)
-                .centerCrop()
-                .placeholder(R.drawable.ic_launcher_background)
+                .fitCenter()
+                .placeholder(R.drawable.ic_launcher_foreground)
                 .into(ivFlag)
+        }
+
+        holder.binding.root.setOnClickListener {
+            Snackbar.make(it, currentCountry.name, Snackbar.LENGTH_LONG).show()
+            val activity = it.context as AppCompatActivity
+            val transaction = activity.supportFragmentManager.beginTransaction()
+            transaction.replace(
+                R.id.fragment_container_view, CountryInfoDetail(currentCountry.name)
+            )
+            transaction.addToBackStack("first_transaction")
+            transaction.commit()
         }
     }
 
