593069a82dceb941b5ba05f4d8576e6b235c25a8
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2022-11-21 14:37:06 +0200

    Implement model

---
+++ API/app/src/main/java/com/example/myapplication/model/MakeupResponse.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication.model
+
+data class MakeupResponse(
+    val api_featured_image: String,
+    val brand: String,
+    val category: String,
+    val created_at: String,
+    val currency: Any,
+    val description: String,
+    val id: Int,
+    val image_link: String,
+    val name: String,
+    val price: String,
+    val price_sign: Any,
+    val product_api_url: String,
+    val product_colors: List<ProductColor>,
+    val product_link: String,
+    val product_type: String,
+    val rating: Double,
+    val tag_list: List<Any>,
+    val updated_at: String,
+    val website_link: String
+)
+
+data class ProductColor(
+    val colour_name: String,
+    val hex_value: String
+)
+
